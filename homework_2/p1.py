77c86852606f876893a65e750a1c644d407fd475
OkChumakova <32899339+OkChumakova@users.noreply.github.com>
2019-08-27 13:41:44 +0300

    Delete p1.py

--- homework_2/p1.py
+++
@@ -1,8 +0,0 @@
-
-v = [[1, 2, 3], [4, 5, 6]]
-print(v[0][0])
-statusOfBoard = [[-1,-1,-1],[-1,-1,-1],[-1,-1,-1],[-1,-1,-1],[-1,-1,-1],[-1,-1,-1],[-1,-1,-1],[-1,-1,-1]]
-statusOfBoard[0][0] +=1
-print(statusOfBoard)
-statusOfBoard[0][0] +=1
-print(statusOfBoard)
